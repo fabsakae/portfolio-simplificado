53b821ae1a3db6a440a40d9401ae69b7c152894d
Fabíola Sakae Shimose Cavalcanti <138534039+fabsakae@users.noreply.github.com>
2024-04-06 22:41:56 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,26 @@
-/*objetivo 1 - quando o usuario clicar no botao 
- // objetivo 2 - esconder o botao mostrar mais
-    // passo 1 -  pegar o botao e esconde lo
+/*Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
+
+        Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele
+
+        Passo 2 - identificar o clique no botão
+        
+        Passo 3 - adicionar a classe "ativo" nos projetos escondidos
+
+    Objetivo 2 - esconder o botão de mostrar mais
+        Passo 1 - pegar o botão e esconder ele
 */
+// Objetivo 1 - quando o usuário clicar no botão de mostrar mais deve abrir os projetos que estão escondidos no html
 // passo 1 -  pegar o botao mostrar mais no js para poder verificar quando o ususario clicar em cima dele
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo');
 
 botaoMostrarProjetos.addEventListener('click', () => {
+    
     // passo 3 - adicionaer a classe ativo nos projetos escondidos
     mostrarMaisProjetos();
+
+    // Objetivo 2 - esconder o botão de mostrar mais
+    // Passo 1 - pegar o botão e esconder ele
     esconderBotao();
 })
 
